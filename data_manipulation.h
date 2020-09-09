3a7d205f8989845eeb9162a2c2e1e53078e518dc
Nikita Shestakov <42568603+nekida@users.noreply.github.com>
2020-09-09 15:16:41 +0300

    Update data_manipulation.h

--- data_manipulation.h
+++ data_manipulation.h
@@ -5,7 +5,7 @@
     #define REVERSE_8_BITS(data) 		(__rbit((uint32_t)data) >> 24)
     #define REVERSE_16_BITS(data)		(__rbit((uint32_t)data) >> 16)
     #define REVERSE_32_BITS(data)		(__rbit((uint32_t)data))
-    #define REVERSE_BYTES_IN_WORD(data)       (__rev(data))
+    #define REVERSE_BYTES_IN_WORD(data)         (__rev(data))
 #elif defined (__ICCARM__)
     #define REVERSE_8_BITS(data) 		(__RBIT((uint32_t)data) >> 24)
     #define REVERSE_16_BITS(data)		(__RBIT((uint32_t)data) >> 16)
