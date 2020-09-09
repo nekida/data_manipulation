3d245880ee3baaf74d2c5b1a91fc7e9d89b7ea5f
Nikita Shestakov <42568603+nekida@users.noreply.github.com>
2020-09-09 15:14:14 +0300

    Update data_manipulation.h

--- data_manipulation.h
+++ data_manipulation.h
@@ -2,10 +2,10 @@
 #define __DATA_MANIPULATION
 
 #if defined   (__CC_ARM)
-    #define REVERSE_8_BITS(data) 		    (__rbit((uint32_t)data) >> 24)
-    #define REVERSE_16_BITS(data)		    (__rbit((uint32_t)data) >> 16)
-    #define REVERSE_32_BITS(data)		    (__rbit((uint32_t)data))
-    #define REVERSE_BYTES_IN_WORD(data)     (__rev(data))
+    #define REVERSE_8_BITS(data) 		 (__rbit((uint32_t)data) >> 24)
+    #define REVERSE_16_BITS(data)		 (__rbit((uint32_t)data) >> 16)
+    #define REVERSE_32_BITS(data)		 (__rbit((uint32_t)data))
+    #define REVERSE_BYTES_IN_WORD(data)  (__rev(data))
 #elif defined (__ICCARM__)
     #define REVERSE_8_BITS(data) 		(__RBIT((uint32_t)data) >> 24)
     #define REVERSE_16_BITS(data)		(__RBIT((uint32_t)data) >> 16)
