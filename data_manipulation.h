91e47790a5fd9f95ea6d0b0682ab3efde3e78d91
Nikita Shestakov <42568603+nekida@users.noreply.github.com>
2020-09-09 15:12:59 +0300

    Add files via upload

---
+++ data_manipulation.h
@@ -0,0 +1,17 @@
+#ifndef __DATA_MANIPULATION
+#define __DATA_MANIPULATION
+
+#if defined   (__CC_ARM)
+    #define REVERSE_8_BITS(data) 			(__rbit((uint32_t)data) >> 24)
+    #define REVERSE_16_BITS(data)			(__rbit((uint32_t)data) >> 16)
+    #define REVERSE_32_BITS(data)			(__rbit((uint32_t)data))
+    #define REVERSE_BYTES_IN_WORD(data)		(__rev(data))
+#elif defined (__ICCARM__)
+    #define REVERSE_8_BITS(data) 			(__RBIT((uint32_t)data) >> 24)
+    #define REVERSE_16_BITS(data)			(__RBIT((uint32_t)data) >> 16)
+    #define REVERSE_32_BITS(data)			(__RBIT((uint32_t)data))
+    #define REVERSE_BYTES_IN_WORD(data)		(__REV(data))
+#endif
+#define REVERSE_BYTES_IN_HALFWORD(data)	(__REV16(data))
+
+#endif
