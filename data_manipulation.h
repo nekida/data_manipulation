051597d6fbd80fd4b5e1f54d9806d8385d3bf5cd
Nikita Shestakov <42568603+nekida@users.noreply.github.com>
2020-09-09 15:16:22 +0300

    Update data_manipulation.h

--- data_manipulation.h
+++ data_manipulation.h
@@ -2,16 +2,16 @@
 #define __DATA_MANIPULATION
 
 #if defined   (__CC_ARM)
-    #define REVERSE_8_BITS(data) 		    (__rbit((uint32_t)data) >> 24)
-    #define REVERSE_16_BITS(data)		    (__rbit((uint32_t)data) >> 16)
-    #define REVERSE_32_BITS(data)		    (__rbit((uint32_t)data))
-    #define REVERSE_BYTES_IN_WORD(data)     (__rev(data))
+    #define REVERSE_8_BITS(data) 		(__rbit((uint32_t)data) >> 24)
+    #define REVERSE_16_BITS(data)		(__rbit((uint32_t)data) >> 16)
+    #define REVERSE_32_BITS(data)		(__rbit((uint32_t)data))
+    #define REVERSE_BYTES_IN_WORD(data)       (__rev(data))
 #elif defined (__ICCARM__)
-    #define REVERSE_8_BITS(data) 		    (__RBIT((uint32_t)data) >> 24)
-    #define REVERSE_16_BITS(data)		    (__RBIT((uint32_t)data) >> 16)
-    #define REVERSE_32_BITS(data)		    (__RBIT((uint32_t)data))
-    #define REVERSE_BYTES_IN_WORD(data)	    (__REV(data))
+    #define REVERSE_8_BITS(data) 		(__RBIT((uint32_t)data) >> 24)
+    #define REVERSE_16_BITS(data)		(__RBIT((uint32_t)data) >> 16)
+    #define REVERSE_32_BITS(data)		(__RBIT((uint32_t)data))
+    #define REVERSE_BYTES_IN_WORD(data)       (__REV(data))
 #endif
-#define REVERSE_BYTES_IN_HALFWORD(data)	    (__REV16(data))
+#define REVERSE_BYTES_IN_HALFWORD(data)     (__REV16(data))
 
 #endif
