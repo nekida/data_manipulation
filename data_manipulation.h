bb8a3bf3f82f5434f9314189a9e25cee88220a1c
Nikita Shestakov <42568603+nekida@users.noreply.github.com>
2020-09-09 15:16:50 +0300

    Update data_manipulation.h

--- data_manipulation.h
+++ data_manipulation.h
@@ -10,7 +10,7 @@
     #define REVERSE_8_BITS(data) 		(__RBIT((uint32_t)data) >> 24)
     #define REVERSE_16_BITS(data)		(__RBIT((uint32_t)data) >> 16)
     #define REVERSE_32_BITS(data)		(__RBIT((uint32_t)data))
-    #define REVERSE_BYTES_IN_WORD(data)       (__REV(data))
+    #define REVERSE_BYTES_IN_WORD(data)         (__REV(data))
 #endif
 #define REVERSE_BYTES_IN_HALFWORD(data)     (__REV16(data))
 
